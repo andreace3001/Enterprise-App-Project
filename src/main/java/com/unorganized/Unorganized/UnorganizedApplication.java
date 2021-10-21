b470930da88e2ee0d9b49acb8b01fcdef25dfb0f
Charlie3169 <77686049+Charlie3169@users.noreply.github.com>
2021-10-20 21:26:31 -0400

    Stuff

---
+++ src/main/java/com/unorganized/Unorganized/UnorganizedApplication.java
@@ -0,0 +1,13 @@
+package com.unorganized.Unorganized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnorganizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnorganizedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unorganized/Unorganized/UnorganizedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unorganized.Unorganized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnorganizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
